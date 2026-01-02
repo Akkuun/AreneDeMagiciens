1a8768e781cd7463b40157f6ee8d675c90314dcf
Crhonopost <nath.souvignet@gmail.com>
2026-01-02 20:10:05 +0100

    world shader

--- jeu/arene-de-magicien/Scenes/levels/object_spawner.gd
+++ jeu/arene-de-magicien/Scenes/levels/object_spawner.gd
@@ -8,8 +8,10 @@ func spawn():
 	add_child(instance)
 
 func _ready() -> void:
-	var instance = scene_to_spawn.instantiate()
+	var instance = scene_to_spawn.instantiate() as XRToolsPickable
 	$View/SubViewport.add_child(instance)
+	instance.freeze = true
+	instance.position = $View/SubViewport/Camera3D.position + Vector3.FORWARD * 0.5
 	spawn()
 
 
