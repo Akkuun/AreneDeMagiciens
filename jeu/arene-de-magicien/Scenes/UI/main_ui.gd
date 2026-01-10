de15ccdfa6324b9a77533d33344bda1685eca5a9
Crhonopost <nath.souvignet@gmail.com>
2026-01-10 14:45:46 +0100

    fix opening settings panel

--- jeu/arene-de-magicien/Scenes/UI/main_ui.gd
+++ jeu/arene-de-magicien/Scenes/UI/main_ui.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@export var settings_panel : Control
 @export var level_selection_available : bool = false
 
 func hide_navigation():
@@ -12,12 +11,12 @@ func _ready() -> void:
 	$Navigation/Levels.visible = level_selection_available
 
 func _on_settings_pressed() -> void:
-	settings_panel.visible = true
+	$GameSettings.visible = true
 	hide_navigation()
 
 
 func _on_game_settings_closed() -> void:
-	settings_panel.visible = false
+	$GameSettings.visible = false
 	show_navigation()
 
 
